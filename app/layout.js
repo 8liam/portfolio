fa53a5addf80427e5a651ecdb8bbdf878b3cfcf1
Liam <liamgrantdev@gmail.com>
2025-07-18 11:55:47 +1000

    feat: add Speed Insights to layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -61,6 +62,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <SpeedInsights />
         <Analytics />
         {children}
       </body>
